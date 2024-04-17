312ed7f7bba9505c04d7da47d8701c3ab7dedacb
samoy <samoy_young@163.com>
2024-04-17 19:04:33 +0800

    fix: remove unnecessary code.

--- test/mock/mock.dart
+++ test/mock/mock.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
-
 import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-@GenerateMocks([SharedPreferences, Platform])
+@GenerateMocks([SharedPreferences])
 void main() {}
